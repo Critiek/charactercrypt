ee4c298552474fbf1fd4c080d95595da8dfa8f6d
James <58063875+Critiek@users.noreply.github.com>
2025-06-05 13:11:06 +0930

    wip: mobile navmenu clickoutside

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { fade } from "svelte/transition";
-    import { onMount } from "svelte";
 
     import {
         Files,
@@ -10,19 +9,13 @@
         PencilRuler,
     } from "@lucide/svelte";
 
+    import { clickOutside } from "./clickOutside";
+
     let open = false;
-
-    onMount(() => {
-        function handleClickOutsideNav() {
-            open = false;
-        }
-
-        document.addEventListener("click", handleClickOutsideNav);
-    })
 </script>
 
 <nav>
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mb-4 mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
             <!-- Logo -->
             <a
@@ -37,20 +30,30 @@
                 <a
                     href="/characters"
                     class="text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >Characters<Files class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
+                    >Characters<Files
+                        class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                    /></a
                 >
-                <a href="/characters" class="text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >Editor<PencilRuler class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
+                <a
+                    href="/characters"
+                    class="text-2xl hover:text-purple-700 dark:hover:text-purple-400"
+                    >Editor<PencilRuler
+                        class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                    /></a
                 >
-                <a href="/characters" class="text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >User<CircleUserRound class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
+                <a
+                    href="/characters"
+                    class="text-2xl hover:text-purple-700 dark:hover:text-purple-400"
+                    >User<CircleUserRound
+                        class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                    /></a
                 >
             </div>
 
             <!-- Mobile Menu Button -->
             <button
                 class="md:hidden focus:outline-none"
-                on:click={() => (open = !open)}
+                onclick={() => (open = !open)}
                 aria-label="Toggle menu"
             >
                 {#if !open}
@@ -65,17 +68,34 @@
     <!-- Mobile Menu -->
     {#if open}
         <!-- <div class="relative"> -->
-            <div transition:fade class="absolute bg-neutral-200/90 dark:bg-neutral-900/90 text-left right-0 md:hidden px-5 pb-5 pt-5 space-y-2">
-                <a href="/characters" class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >Characters<Files class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
-                >
-                <a href="/characters" class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >Editor<PencilRuler class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
-                >
-                <a href="/characters" class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
-                    >User<CircleUserRound class="ml-2 inline-flex text-purple-700 dark:text-purple-400" /></a
-                >
-            </div>
+        <div
+            use:clickOutside
+            onoutclick={() => {open = false;}}
+            transition:fade
+            class="absolute bg-neutral-200/90 dark:bg-neutral-900/90 text-left right-0 md:hidden px-5 pb-5 pt-5 space-y-2"
+        >
+            <a
+                href="/characters"
+                class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
+                >Characters<Files
+                    class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                /></a
+            >
+            <a
+                href="/characters"
+                class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
+                >Editor<PencilRuler
+                    class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                /></a
+            >
+            <a
+                href="/characters"
+                class="flex items-center justify-between text-2xl hover:text-purple-700 dark:hover:text-purple-400"
+                >User<CircleUserRound
+                    class="ml-2 inline-flex text-purple-700 dark:text-purple-400"
+                /></a
+            >
+        </div>
         <!-- </div> -->
     {/if}
 </nav>
