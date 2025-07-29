21862ecc5af483e29f7f90a786a5a9b8f1dd6a34
James <jamesvsomeren@gmail.com>
2025-07-29 19:59:51 +0930

    working on uuid test

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -13,14 +13,9 @@ async fn main() {
         .route("/user/{id}", get(return_id))
         .route("/uuid", get(new_uuid));
 
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-
-
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-    axum::serve(listener, app2).await.unwrap();
 }
 
 async fn return_id(Path(user_id): Path<u64>) -> impl IntoResponse {
