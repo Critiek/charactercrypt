45a629ddf0635354d50ce4b038d1d895ee7798a5
James <58063875+Critiek@users.noreply.github.com>
2025-07-29 18:27:06 +0930

    wip: testing uuid crate

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use uuid::Uuid;
 
 const IP_PORT: &str = "0.0.0.0:3000";
 
@@ -9,13 +10,23 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/user/{id}", get(return_id));
+        .route("/user/{id}", get(return_id))
+        .route("/uuid", get(new_uuid));
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app2).await.unwrap();
 }
 
 async fn return_id(Path(user_id): Path<u64>) -> impl IntoResponse {
     format!("You tried to get {}", user_id)
+}
+
+async fn new_uuid() -> impl IntoResponse {
+    Uuid::new_v4().to_string()
 }
